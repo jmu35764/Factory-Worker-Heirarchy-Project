c81c8dc3b9879b34ca8f99a862ac1969f58d901b
jm underwood <jm.underwood@hotmail.com>
2026-05-03 11:44:57 -0400

    Add project files.

---
+++ Factory Worker Heirarchy Project/Employee.h
@@ -0,0 +1,26 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string>
+using namespace std;
+
+class Employee
+{
+private:
+	string Emp_Name;
+	int Emp_Num;
+	string Hire_date;
+
+public:
+	//Constructor
+	Employee(string e_name = " ", int e_num = 0, string H_date = " ");
+
+
+
+
+
+
+};
+
+
+#endif
