448c08c54ce2b50247d8267545ca34df14bcfe06
jm underwood <jm.underwood@hotmail.com>
2026-05-03 22:51:26 -0400

    Finished TeamLeader.cpp. Now going to test it out.

--- Factory Worker Heirarchy Project/TeamLeader.cpp
+++ Factory Worker Heirarchy Project/TeamLeader.cpp
@@ -8,6 +8,30 @@ TeamLeader::TeamLeader(string e_name, int e_num, string H_date, int s, double h_
 						: ProductionWorker(e_name, e_num, H_date, s, h_pay)
 {
 	MonthBonus = m_bon;
-	RegTrainingHours = rtg;
+	RegTrainingHours = rth;
+	AttendTrainHours = ath;
+}
 
+void TeamLeader::setMonthBonus(double m_bon)
+{
+	MonthBonus = m_bon;
+}
+
+void TeamLeader::setRegTrainingHours(int rth)
+{
+	RegTrainingHours = rth;
+}
+
+void TeamLeader::setAttendTrainHours(int ath)
+{
+	AttendTrainHours = ath;
+}
+
+void TeamLeader::printTeamLeaderInfo() const
+{
+	printProdWorkInfo;
+
+	cout << left << setw(18) << "Monthly Bonus: $" << MonthBonus << endl;
+	cout << left << setw(27) << "Regular Training Hours: " << RegTrainingHours << endl;
+	cout << left << setw(27) << "Attended Training Hours: " << AttendTrainHours << endl;
 }
