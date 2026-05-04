7a90d6e5e3e3e9152b1f17c37a2f61eb157aacf6
jm underwood <jm.underwood@hotmail.com>
2026-05-04 17:48:27 -0400

    Added code for production worker test

--- Factory Worker Heirarchy Project/Factory Worker Heirarchy Project.cpp
+++ Factory Worker Heirarchy Project/Factory Worker Heirarchy Project.cpp
@@ -13,29 +13,50 @@ int main()
 {
 	//****** EMPLOYEE CLASS TEST ******//
 
-	cout << "EMPLOYEE CLASS TEST" << endl;
+	cout << "///// EMPLOYEE CLASS TEST /////" << endl << endl;
 	
+	
+	cout << "Current Employee Info" << endl;
+
 	Employee Emp("John", 1234, "10 / 05 / 2001");
 
 	Emp.printEmpInfo();
 
+	// Setter TEST
 	Emp.setName("Miles Morales");
 	Emp.setEmpNum(5678);
 	Emp.setHireDate("06/06/2001");
 
-
+	// Getter TEST
+	cout << endl;
 	cout << "New Employee Name: " <<Emp.getName() << endl;
 	cout << "New Employee Number: " << Emp.getEmpNum() << endl;
 	cout << "New Empoyee Hire Date: " << Emp.getHireDate() << endl;
 
 
-	cout << endl;
+	cout << endl << "New Employee Info" << endl;
 	Emp.printEmpInfo();
 
-	ProductionWorker pw("Mason", 1001, "11/11/2011", 2, 23.00);
+
+	//****** PRODUCTION WORKER CLASS TEST ******//
+
+	cout << "///// PRODUCTION WORKER CLASS TEST /////" << endl << endl;
+
+	cout << "DAY SHIFT WORKER" << endl;
+	
+	ProductionWorker pw1("Mason", 1001, "11/11/2011", 1, 23.00);
 
 	cout << endl;
-	pw.printProdWorkInfo();
+	pw1.printProdWorkInfo();
+
+	cout << "NIGHT SHIFT WORKER" << endl;
+
+	ProductionWorker pw2("Shane Ta", 5050, "01/01/2012", 2, 23.00);
+
+	cout << endl;
+	pw2.printProdWorkInfo();
+
+
 
 	ShiftSupervisor ss("Andrew Myers", 2020, "04/15/2016", 4000.00, 500.00);
 
